5e48e3a91eaf0ac32ff688c1a31f1929d1900b56
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2025-07-08 14:01:59 +0200

    feat: no storage for vercel

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,6 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
 import { companyWorkflow } from './workflows/company-information-workflow';
 import { companyAgent } from './agents/company-agent';
 import { weatherAgent } from './agents/weather-agent';
@@ -10,10 +9,6 @@ import { VercelDeployer } from "@mastra/deployer-vercel";
 export const mastra = new Mastra({
   workflows: { companyWorkflow },
   agents: { companyAgent, weatherAgent },
- /* storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),*/
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
