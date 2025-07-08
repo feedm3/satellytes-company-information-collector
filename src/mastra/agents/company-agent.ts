d84393c380ef230e859a00e383b0373cfc8b0cd4
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2025-07-08 14:06:55 +0200

    feat: remove data

--- src/mastra/agents/company-agent.ts
+++ src/mastra/agents/company-agent.ts
@@ -1,9 +1,6 @@
 import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 
-import { Memory } from '@mastra/memory';
-import { LibSQLStore } from '@mastra/libsql';
-
 import { companyFoundingYearTool } from '../tools/company-founding-year-tool';
 import { companyRevenueTool } from '../tools/company-revenue-tool';
 import { SummarizationMetric } from "@mastra/evals/llm";
@@ -26,11 +23,6 @@ export const companyAgent = new Agent({
 `,
   model: openai('gpt-4o-mini'),
   tools: { companyFoundingYearTool, companyRevenueTool },
-  memory: new Memory({
-    storage: new LibSQLStore({
-      url: 'file:../mastra.db', // path is relative to the .mastra/output directory
-    }),
-  }),
   evals: {
     summarization: new SummarizationMetric(openai('gpt-4o-mini')),
     contentSimilarity: new ContentSimilarityMetric(),
